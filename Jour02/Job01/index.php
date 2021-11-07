7d8c50f4489db7a3d669670c3ee109527c62e9b5
abdou-mbae1 <abdou.mbae@laplateforme.io>
2021-11-07 20:59:17 +0100

    projet fini

---
+++ Jour02/Job01/index.php
@@ -0,0 +1,20 @@
+<?php
+$x = 0;
+            
+ do{
+echo '<br>Nombre ' .$x. '</br>';
+$x++;
+}while($x <= 41);
+            
+if ($x == 42)
+{
+    echo '<br><b><u>Nombre' .$x. '</u></b></br>';
+    $x++;
+}
+
+
+do{
+echo '<br>Nombre ' .$x. '<br>';
+$x++;}
+while($x <= 1337);
+?>
---
+++ Jour03/job07/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$str = "Certaines choses changent, et d'autres
+ne changeront jamais.";
+$newstr = ""; 
+var_dump($str);
+for($i=0; isset($str[$i]); $i++)
+{
+    
+}
+var_dump($i);
